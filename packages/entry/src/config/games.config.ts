d02021ed4657cf59d4497bcc8efd50af7ac313d4
humandetail.com <512412344@qq.com>
2022-11-19 18:08:30 +0800

    feat: add link-game

--- packages/entry/src/config/games.config.ts
+++ packages/entry/src/config/games.config.ts
@@ -9,5 +9,10 @@ export const games: GameConfig[] = [
     path: '/mini-games/games/mine-sweeper/index.html',
     name: '扫雷',
     cover: ''
+  },
+  {
+    path: '/mini-games/games/link-game/index.html',
+    name: '连连看',
+    cover: ''
   }
 ]
