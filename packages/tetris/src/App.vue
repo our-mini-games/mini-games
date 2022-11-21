faebf81237ff37b46e7aaf40f01bec526252a149
Humandetail <39545747+humandetail@users.noreply.github.com>
2022-11-21 17:43:57 +0800

    feat: add tetris project

---
+++ packages/tetris/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tetris">
+    TETRIS {{ a }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const a = ref(1)
+</script>
---
+++ packages/tetris/src/config/game.config.ts
@@ -0,0 +1,32 @@
+export const wrapperSize = {
+  row: 20,
+  column: 10
+}
+
+export const defaultLevel = 1
+
+// 方块元素
+export enum Tetrominos {
+  // ❒❒❒❒
+  I,
+  //  ❒❒
+  // ❒❒
+  S,
+  // ❒❒
+  //  ❒❒
+  Z,
+  // ❒
+  // ❒
+  // ❒❒
+  L1,
+  //  ❒
+  //  ❒
+  // ❒❒
+  L2,
+  // ❒❒
+  // ❒❒
+  O,
+  //  ❒
+  // ❒❒❒
+  T
+}
