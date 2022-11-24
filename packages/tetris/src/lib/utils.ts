caf805b0eea8f09e40bd8c1e2546f9cefdfdf85d
humandetail.com <512412344@qq.com>
2022-11-24 20:58:23 +0800

    fix: collision detection

--- packages/tetris/src/lib/utils.ts
+++ packages/tetris/src/lib/utils.ts
@@ -21,31 +21,8 @@ export const createTetris = (tetrominos: Tetrominos, type: TetrisType): Tetris =
 /**
  * 元素是否触底
  * @param activeCoordinates - 当前活跃的元素
- * @param buildingHighestPoints - 已搭建好的建筑最高点
+ * @param building - 已搭建好的建筑
  */
-export const isReachBottom = (activeCoordinates: Tetris['coordinates'], buildingHighestPoints: Coordinate[]): boolean => {
-  const lowestPoints: Coordinate[] = []
-
-  activeCoordinates.forEach(coordinate => {
-    const colItemIdx = lowestPoints.findIndex(item => item.x === coordinate.x)
-    if (colItemIdx === -1) {
-      lowestPoints.push(coordinate)
-    } else if (coordinate.y > lowestPoints[colItemIdx].y) {
-      lowestPoints.splice(colItemIdx, 1, coordinate)
-    }
-  })
-
-  const currentColumnHighestPoints = buildingHighestPoints.filter(item => lowestPoints.find(l => l.x === item.x))
-
-  if (currentColumnHighestPoints.length === 0) {
-    // 0 -> wrapperSize.row - 1
-    // 当前最低点触底
-    return lowestPoints.some(item => item.y >= wrapperSize.row - 1)
-  }
-
-  return currentColumnHighestPoints.some(h => {
-    const sameColItem = lowestPoints.find(l => l.x === h.x)
-    if (!sameColItem) return false
-    return Math.abs(h.y - sameColItem.y) === 1
-  })
+export const isReachBottom = (activeCoordinates: Tetris['coordinates'], building: Coordinate[]): boolean => {
+  return activeCoordinates.some(item => (item.y > wrapperSize.row - 1) || building.find(b => b.x === item.x && b.y === item.y))
 }
