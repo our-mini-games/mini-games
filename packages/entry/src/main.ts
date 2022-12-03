1a4a4d837cac212934e638991d648f22d8651db5
humandetail.com <512412344@qq.com>
2022-12-03 11:42:12 +0800

    fix: env vars

--- packages/entry/src/main.ts
+++ packages/entry/src/main.ts
@@ -5,6 +5,6 @@ import { games } from './config'
 
 const app = document.querySelector('#app')!
 
-const oGames = createGameList(games, import.meta.env.VITE_PATHNAME)
+const oGames = createGameList(games, import.meta.env.VITE_APP_PATHNAME)
 
 app.appendChild(oGames)
