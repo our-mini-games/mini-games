e3ad48e89477f1637b85d144489eb5af6322146b
banlify <banlify@163.com>
2022-12-30 15:31:36 +0800

    fix: 修复俄罗斯方块过场声音无法停止 #63

--- packages/tetris/src/composables/useAudio.ts
+++ packages/tetris/src/composables/useAudio.ts
@@ -25,8 +25,6 @@ export default (): AudioReturnType => {
   const rotate = ref(noop)
   const stop = ref(noop)
 
-  // @todo 记录所有 source 进行 stop
-
   onMounted(() => {
     const context = new AudioContext()
     const req = new XMLHttpRequest()
@@ -46,6 +44,8 @@ export default (): AudioReturnType => {
             return source
           }
 
+          const audioSourcePool: Map<'start' | 'clear' | 'fall' | 'gameover' | 'rotate' | 'move', AudioBufferSourceNode > = new Map()
+
           killStart.value = (): void => {
             // 游戏开始的音乐只播放一次
             start.value = () => {}
@@ -54,27 +54,27 @@ export default (): AudioReturnType => {
           // 游戏开始
           start.value = () => {
             killStart.value()
-            getSource().start(0, 3.7202, 3.6224)
+
+            audioSourcePool.set('start', getSource()).get('start')!.start(0, 3.7202, 3.6224)
           }
 
           // 消除
-          clear.value = () => getSource().start(0, 0, 0.7675)
+          clear.value = () => audioSourcePool.set('clear', getSource()).get('clear')!.start(0, 0, 0.7675)
 
           // 立即下落
-          fall.value = () => getSource().start(0, 1.2558, 0.3546)
+          fall.value = () => audioSourcePool.set('fall', getSource()).get('fall')!.start(0, 1.2558, 0.3546)
 
           // 游戏结束
-          gameover.value = () => getSource().start(0, 8.1276, 1.1437)
+          gameover.value = () => audioSourcePool.set('gameover', getSource()).get('gameover')!.start(0, 8.1276, 1.1437)
 
           // 变换
-          rotate.value = () => getSource().start(0, 2.2471, 0.0807)
+          rotate.value = () => audioSourcePool.set('rotate', getSource()).get('rotate')!.start(0, 2.2471, 0.0807)
 
           // 左右移动 / 按键声
-          move.value = () => getSource().start(0, 2.9088, 0.1437)
+          move.value = () => audioSourcePool.set('move', getSource()).get('move')!.start(0, 2.9088, 0.1437)
 
           // 停止播放
-          // @todo
-          stop.value = () => getSource().stop()
+          stop.value = () => audioSourcePool.get('start')!.stop()
         },
         error => {
           console.error('音频读取错误', error)
