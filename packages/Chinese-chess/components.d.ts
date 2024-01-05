61457bf7fa128866f6fd35a27e7f0b07f5509484
laihanbin <512412344@qq.com>
2024-01-05 15:11:24 +0800

    fix: 增加聊天服务

--- packages/Chinese-chess/components.d.ts
+++ packages/Chinese-chess/components.d.ts
@@ -7,7 +7,10 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ABadge: typeof import('ant-design-vue/es')['Badge']
     AButton: typeof import('ant-design-vue/es')['Button']
+    AInput: typeof import('ant-design-vue/es')['Input']
+    AInputGroup: typeof import('ant-design-vue/es')['InputGroup']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ASpin: typeof import('ant-design-vue/es')['Spin']
     CampSeat: typeof import('./src/components/ChessMain/CampSeat.vue')['default']
@@ -19,6 +22,7 @@ declare module '@vue/runtime-core' {
     LeftOutlined: typeof import('@ant-design/icons-vue')['LeftOutlined']
     Modal: typeof import('./src/components/Message/Modal.vue')['default']
     ModeSelector: typeof import('./src/components/ModeSelector.vue')['default']
+    SoundOutlined: typeof import('@ant-design/icons-vue')['SoundOutlined']
     Spectator: typeof import('./src/components/ChessMain/Spectator.vue')['default']
     StatusWrapper: typeof import('./src/components/ChessMain/StatusWrapper.vue')['default']
   }
