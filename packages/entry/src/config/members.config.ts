032bf181606e7b199445012d659909bb6d1ac085
laihanbin <512412344@qq.com>
2024-04-17 15:07:23 +0800

    fix: add mscodesocool as a member

--- packages/entry/src/config/members.config.ts
+++ packages/entry/src/config/members.config.ts
@@ -1,19 +1,28 @@
 export interface MemberConfig {
   avatar: string
   name: string
+  homepage: string
 }
 
 export const members: MemberConfig[] = [
   {
     avatar: 'https://avatars.githubusercontent.com/u/39545747?v=4',
-    name: 'Detail'
+    name: 'Detail',
+    homepage: 'https://github.com/humandetail'
   },
   {
     avatar: 'https://avatars.githubusercontent.com/u/47918504?v=4',
-    name: 'Chestnut'
+    name: 'Chestnut',
+    homepage: 'https://github.com/libondev'
   },
   {
     avatar: 'https://avatars.githubusercontent.com/u/71477878?v=4',
-    name: 'Kkk'
+    name: 'Kkk',
+    homepage: 'https://github.com/Stan-BK'
+  },
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/93576208?v=4',
+    name: 'Mscodesocool',
+    homepage: 'https://github.com/mscodesocool'
   }
 ]
